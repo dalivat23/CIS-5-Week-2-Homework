0fa5775995ad156694b83c29ff7c669ddbb1b4dc
dalivat23 <dalivat23@gmail.com>
2026-09-08 05:07:25 +0000

    Added Comments going over parts of the code, Added more \n statements to clean up output

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 // CIS 5 Week 02 · Structure polish
 
 int main() {
-  // Block 1 — who you are (from Lab 2)
-  std::cout << "=== About me ===\n";
+  // Seperated lines in code by one space and seperated blocks by two spaces for easier reading
+  // Use of \n in order to keep the outputed blocks and lines to be easier to read
+  // Block 1 — Basic About Me statements regarding Name, Interest, CIS-5 Goal, Tool Path Used, and Fun Fact
+  std::cout << "\n=== About me ===\n";
 
   std::cout << "Name: Tristan Daliva\n";
   
@@ -15,17 +17,17 @@ int main() {
   
   std::cout << "Tool path: GitHub Codespaces\n";
   
-  std::cout << "Fun fact: I usually work better later in the day or at night\n";
+  std::cout << "Fun fact: I usually work better later in the day or at night\n\n";
 
  
-  // Block 2 — this week's goals (from outcomes)
+  // Block 2 — 3 meaningful goals that are obtainable and within realistic parameters for this semester
   std::cout << "=== Week 02 goals ===\n";
-  // Following Three Lines: Meaningful goals that are realistic
+
   std::cout << "Be able to add meaninful comments to my lines of code\n";
   
   std::cout << "Be able write my own cout lines\n";
   
-  std::cout << "Be able to use trial and error to figure out my mistakes\n";
+  std::cout << "Be able to use trial and error to figure out my mistakes\n\n";
   
 
 
