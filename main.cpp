35643a7143aeb2a0af76927b459712e19701789c
dalivat23 <dalivat23@gmail.com>
2026-09-08 04:56:31 +0000

    Added Required Cout lines and wrote out the correct prompts

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 
-// Practice 2 — Your Name
+// Practice 2 — Tristan Daliva
 // CIS 5 Week 02 · Structure polish
 
 int main() {
   // Block 1 — who you are (from Lab 2)
   std::cout << "=== About me ===\n";
-  // TODO: 3+ labeled lines
 
+  std::cout << "Name: Tristan Daliva\n";
+  
+  std::cout << "Interest: Video Games\n";
+  
+  std::cout << "CIS 5 goal: Learn something new every week about coding and have fun doing it \n";
+  
+  std::cout << "Tool path: GitHub Codespaces\n";
+  
+  std::cout << "Fun fact: I usually work better later in the day or at night\n";
+
+ 
   // Block 2 — this week's goals (from outcomes)
   std::cout << "=== Week 02 goals ===\n";
-  // TODO: list 3 goals as complete sentences
+  // Following Three Lines: Meaningful goals that are realistic
+  std::cout << "Be able to add meaninful comments to my lines of code\n";
+  
+  std::cout << "Be able write my own cout lines\n";
+  
+  std::cout << "Be able to use trial and error to figure out my mistakes\n";
+  
+
 
   return 0;
 }
